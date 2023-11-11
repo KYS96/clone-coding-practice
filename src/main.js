d13b81576aafcfe8b88f947a1b217615e9ed78eb
KYS96 <junjo123@naver.com>
2023-11-11 14:34:13 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -27,4 +27,17 @@ document.addEventListener('scroll', () => {
   } else {
     arrowUp.style.opacity = 0;
   }
+});
+
+//Navbar 토글버튼 클릭처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle  = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 })
+
+//Navbar 메뉴 클릭 시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+})
+
